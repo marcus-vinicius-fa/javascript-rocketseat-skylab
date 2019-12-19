b21822726e7fd3ff53915962897b5226cea76dee
marcus-vinicius-fa <macusvinicius_ti@outlook.com>
2019-12-19 17:32:40 -0300

    Adicionei projetos ao README.md

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 Este repositório contém os arquivos gerados nos meus estudos de JavaScript básico, features novas (ES6+) e NodeJS, utilizando o material da Rocketseat na plataforma [Skylab](https://skylab.rocketseat.com.br/).
 
-Anotações úteis:
+### Mini-projetos (aplicações) desenvolvidos nos cursos:
+
+- [ToDos](skylab/src/javascript/3-app-de-todos/todos.js)
+>_**Habilidades praticadas:** manipulação da DOM, funcões, JSON.parse e .stringfy, e programação/scripts no geral_
+
+- [Search Repositories](skylab/src/javascript/4-js-assincrono/exercicios/exercicio.js)
+>_**Habilidades praticadas:** uso XMLHttpRequest, Promises, uso da lib Axios, manipulação da DOM, funcões e programação/scripts no geral_
+
+### Anotações úteis:
 
 - [ESLint, como?](markdown/eslint.md)
 - [Projeto com ES6+](markdown/es6.md)
--- skylab/src/javascript/4-js-assincrono/exercicios/exercicio.js
+++ skylab/src/javascript/4-js-assincrono/exercicios/exercicio.js
@@ -1,3 +1,5 @@
+// const axios = require(' axios ');
+
 var bodyElement = document.querySelector('body');
 // var bodyElement = document.getElementsByTagName('body')[0];
 
@@ -14,7 +16,7 @@ inputElement.placeholder = 'Nome do usuário';
 // inputElement.setAttribute('placeholder', 'Nome do usuário'); // Pq tbm dá pra fazer assim?
 
 var btnElement = document.createElement('button');
-btnElement.setAttribute('onclick', 'getUser()');
+btnElement.setAttribute('onclick', 'getRepositories()');
 var btnText = document.createTextNode('Buscar');
 btnElement.appendChild(btnText);
 
@@ -37,7 +39,7 @@ bodyElement.appendChild(divElement2)
 
 var xhr = new XMLHttpRequest()
 
-var promiseUser = function () {
+var SearchRepositoriesWithXHR = function () {
 
 	return new Promise(function (resolve, reject) {
 		divElement2.innerHTML = '';
@@ -58,7 +60,7 @@ var promiseUser = function () {
 			if (xhr.readyState === 4) {
 				pElement.innerHTML = ''
 				if (xhr.status === 200) {
-					response = JSON.parse(xhr.responseText);
+					var response = JSON.parse(xhr.responseText);
 					console.log(response)
 
 					// divElement2.innerHTML = '';
@@ -87,8 +89,56 @@ var promiseUser = function () {
 	})
 }
 
-function getUser() {
-	promiseUser()
+//======================================================================================
+
+// FIXME: Erro na função usando o axios
+// Com AXIOS
+
+var SearchRepositoriesWithAxios = function () {
+
+	divElement2.innerHTML = '';
+
+	pElement = document.createElement('p')
+	pText = document.createTextNode('Carregando...')
+	pElement.appendChild(pText)
+
+	divElement2.appendChild(pElement)
+
+	var userName = inputElement.value;
+	inputElement.value = '';
+	axios.get('https://api.github.com/users/' + userName + '/repos')
+		.then(function (response) {
+			pElement.innerHTML = ''
+			var response = response.data;
+			console.log(response);
+
+			var hElement = document.createElement('h3')
+			var hText = document.createTextNode(userName)
+			var ulElement = document.createElement('ul')
+			hElement.appendChild(hText)
+			ulElement.appendChild(hElement)
+
+			divElement2.appendChild(ulElement)
+
+			for (var i = 0; i <= response.length; i++) {
+				var repository = response[i].name
+				var liElement = document.createElement('li');
+				var liText = document.createTextNode(repository)
+				liElement.appendChild(liText)
+				ulElement.appendChild(liElement);
+			}
+			return response
+		})
+		.catch(function (err) {
+			console.log(err)
+			return alert('Erro na requisição!')
+		})
+}
+
+// Para usar Axios comente essa function e descomente a próxima function (em "aDescomente aqui")
+
+function getRepositories() {
+	SearchRepositoriesWithXHR()
 		.then(function (response) {
 			console.log(response)
 		})
@@ -97,4 +147,8 @@ function getUser() {
 		})
 }
 
-// Com AXIOS:
+// Descomente aqui:
+
+// function getRepositories() {
+// 	SearchRepositoriesWithAxios()
+// }
